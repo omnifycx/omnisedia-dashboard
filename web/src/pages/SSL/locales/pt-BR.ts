6b5202158bd4b42d96a6a00c43799b2e23108a46
Maximiliano Veiga <maxsantosneto@gmail.com>
2022-02-21 16:26:34 -0300

    Fix missing languages

---
+++ web/src/pages/SSL/locales/pt-BR.ts
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+export default {
+  'page.ssl.form.itemLabel.expireTime': 'Tempo de expiração',
+  'page.ssl.form.itemLabel.cert': 'Conteúdo do certificado',
+  'page.ssl.form.itemRuleMessage.certValueLength':
+    'O Conteúdo do Certificado requer pelo menos 128 caracteres',
+  'page.ssl.form.itemLabel.privateKey': 'Chave privada',
+  'page.ssl.form.itemRuleMessage.privateKeyLength':
+    'A chave privada requer pelo menos 128 caracteres',
+
+  'page.ssl.button.uploadCert': 'Carregar certificado',
+
+  'page.ssl.form.itemLabel.way': 'Caminho',
+  'page.ssl.select.placeholder.selectCreateWays': 'Por favor, selecione criar formas',
+  'page.ssl.selectOption.input': 'Input',
+  'page.ssl.upload': 'Carregar',
+
+  'page.ssl.notification.updateCertEnableStatusSuccessfully':
+    'Atualizar o status de habilitação do certificado com sucesso',
+  'page.ssl.list': 'Lista SSL',
+  'page.ssl.list.expirationTime': 'Tempo de expiração',
+  'page.ssl.list.ifEnable': 'Se habilitado',
+  'page.ssl.list.periodOfValidity': 'Período de validade',
+  'page.ssl.steps.stepTitle.completeCertInfo': 'Informações completas do certificado',
+  'component.ssl.removeSSLSuccess': 'Remover SSL de destino com sucesso',
+  'component.ssl.removeSSLItemModalContent': 'Você vai remover este item!',
+
+  'component.ssl.description':
+    'O certificado é usado pelo gateway para processar solicitações criptografadas, que serão associadas ao SNI e vinculadas ao nome do host na Rota.',
+  'component.ssl.fields.cert.required': 'Por favor, insira o certificado',
+  'component.ssl.fields.key.required': 'Por favor, insira a chave',
+};
